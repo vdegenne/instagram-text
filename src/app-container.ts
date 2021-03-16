a465a6d88da15cdfbb88d004cb197e81a2bb8021
valentin degenne <vdegenne@gmail.com>
2021-03-16 19:27:12 +0100

    asdf

--- src/app-container.ts
+++ src/app-container.ts
@@ -1,7 +1,9 @@
 import { LitElement, html, customElement, property, css } from "lit-element";
 import '@material/mwc-textarea'
 import '@material/mwc-slider'
+import '@material/mwc-button'
 import 'vanilla-colorful'
+import html2canvas from 'html2canvas'
 
 @customElement('app-container')
 class AppContainer extends LitElement {
@@ -64,6 +66,15 @@ class AppContainer extends LitElement {
       value="${this.fontSize}" @input="${e => this.fontSize = e.target.value}"></mwc-slider>
     <div>font-size: ${this.fontSize}px</div>
 
+    <mwc-button raised label="save"
+      @click="${_ => this.save()}"></mwc-button>
     `
   }
+
+  async save() {
+    await html2canvas(this.shadowRoot!.querySelector<HTMLElement>('#square')!);
+  }
 }
+
+
+window.html2canvas = html2canvas;
